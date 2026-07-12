782d0d7ac1f28a1645b0ff537f2d8530b7f546ae
abdo-bahi <abdo.bahi.bahi@etu.univ-blida.dz>
2026-07-12 22:13:52 +0100

    initiating shadcn , setting pages tree and adding a side bar component

---
+++ src/app/(dashboard)/page.tsx
@@ -0,0 +1,15 @@
+import { getAllWilayas } from "@/dal/wilaya.dal";
+
+export default async function Dashboard() {
+  const wilayas = await getAllWilayas();
+  return (
+    <div>
+      <h1>here are wilayas : </h1>
+    <ul>
+      {wilayas?.map((w) => {
+        return <li key={w.id}>{w.name} - {w.code}</li>
+      })}
+    </ul>
+    </div>
+  );
+}
