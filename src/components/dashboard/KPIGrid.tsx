0c1bf09c610e5da35c3025cc85e3a46ee214c150
abdo-bahi <abdo.bahi.bahi@etu.univ-blida.dz>
2026-07-25 13:49:27 +0100

    getting actuel sensor data avg in dashboard

--- src/components/dashboard/KPIGrid.tsx
+++ src/components/dashboard/KPIGrid.tsx
@@ -1,51 +1,53 @@
- "use client"
-import { Wifi, Droplets, Thermometer, TriangleAlert } from "lucide-react"
-import { KPICard } from "@/components/dashboard/KPICard"
-import { useFieldStore } from "@/store/field-store"
+"use client";
+import { Wifi, Droplets, Thermometer, TriangleAlert } from "lucide-react";
+import { KPICard } from "@/components/dashboard/KPICard";
+import { useFieldStore } from "@/store/field-store";
+import { sensorRouter } from "@/server/routers/sensors.router";
+import { trpc } from "@/lib/trpc/client";
 
 export function KPIGrid() {
   const { selectedField } = useFieldStore();
-
+  const { data: sensorAvgReadings, isLoading } =
+    trpc.sensor.getLatestPerField.useQuery(
+      {
+        irrigationFieldId: selectedField?.id ?? "",
+      },
+      { enabled: !!selectedField?.id }
+    );
+  if (isLoading) return <div>Loading...</div>;
   return (
-  <div>
-    {/* Header row */}
-    <div className="flex items-center gap-3 mb-4">
-      <h2 className="text-[18px] font-bold text-[#1A2E22]">
-        {selectedField?.name ?? ""}
-      </h2>
-
+    <div>
+      {/* Header row */}
+      <div className="flex items-center gap-3 mb-4">
+        <h2 className="text-[18px] font-bold text-[#1A2E22]">
+          {selectedField?.name ?? ""}
+        </h2>
+      </div>
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4">
+        <KPICard
+          title="MCUs Actifs"
+          value="2/2"
+          subtitle="dans cette parcelle"
+          icon={Wifi}
+          color="green"
+        />
+        {sensorAvgReadings.map((sensor: any) => {
+          return (
+            <KPICard
+              title={sensor?.sensorType?.name}
+              value={sensor?.average}
+              subtitle={`las updated at ${sensor?.lastReadAt}`}
+              icon={
+                sensor?.sensorType?.name === "soilMoisture"
+                  ? Droplets
+                  : Thermometer
+              }
+              //to use min irrigation later
+              color={sensor?.average > 30 ? "green" : "red"}
+            />
+          );
+        })}
+      </div>
     </div>
-    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4">
-      <KPICard
-        title="MCUs Actifs"
-        value="2/2"
-        subtitle="dans cette parcelle"
-        icon={Wifi}
-        color="green"
-      />
-      <KPICard
-        title="Humidité Moy."
-        value="71%"
-        subtitle="tous capteurs"
-        icon={Droplets}
-        color="green"
-      />
-      <KPICard
-        title="Température Moy."
-        value="28.8°C"
-        subtitle="capteurs actifs"
-        icon={Thermometer}
-        color="amber"
-      />
-      <KPICard
-        title="Alertes Actives"
-        value="0"
-        subtitle="Tout est normal"
-        icon={TriangleAlert}
-        color="green"
-      />
-    </div>
-    </div>
-
   );
 }
--- src/server/routers/_app.router.ts
+++ src/server/routers/_app.router.ts
@@ -1,8 +1,10 @@
 import { router } from "../trpc"
 import { fieldRouter } from "./field.router"
+import { sensorRouter } from "./sensors.router"
 
 export const appRouter = router({
   field: fieldRouter,
+  sensor: sensorRouter
 })
 
 export type AppRouter = typeof appRouter
